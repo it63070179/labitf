15d678b23a0248132720fa29fd59a62e2538401e
it63070179 <73689108+it63070179@users.noreply.github.com>
2020-11-19 19:34:52 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -1,9 +1,15 @@
 <html>
 <head>
-<title>ITF Lab</title>
+    <title>ITF Lab</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<?php
+    <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'labit63070179.mysql.database.azure.com', 'suwannapong@labit63070179', 'b0826420742@', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
@@ -12,26 +18,32 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-  </tr>
-<?php
+    <table width="600" border="1" class="table table-dark table-striped">
+        <tr>
+            <th width="100">
+                <div align="center">Name</div>
+            </th>
+            <th width="350">
+                <div align="center">Comment </div>
+            </th>
+            <th width="150">
+                <div align="center">Link </div>
+            </th>
+        </tr>
+        <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-  </tr>
-<?php
+        <tr>
+            <td><?php echo $Result['Name'];?></div></td>
+            <td><?php echo $Result['Comment'];?></td>
+            <td><?php echo $Result['Link'];?></td>
+        </tr>
+        <?php
 }
 ?>
-</table>
-<?php
+    </table>
+    <?php
 mysqli_close($conn);
 ?>
 </body>
